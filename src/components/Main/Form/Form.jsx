3bbaff676e2e9b33fbea39bbfcb5cbc75dd65bbd
Safnaj <safnaj99@gmail.com>
2021-01-05 18:40:53 +0530

    Form Works with Context

--- src/components/Main/Form/Form.jsx
+++ src/components/Main/Form/Form.jsx
@@ -1,18 +1,29 @@
-import React from "react";
-import {
-  TextField,
-  Typography,
-  Grid,
-  Button,
-  FormControl,
-  InputLabel,
-  Select,
-  MenuItem,
-} from "@material-ui/core";
+import React, { useState, useContext } from "react";
+import { TextField, Typography, Grid, Button, FormControl, InputLabel, Select, MenuItem } from "@material-ui/core";
+import { v4 as uuidv4 } from "uuid";
+import { ExpenseTrackerContext } from '../../../context/context';
 import useStyles from "./styles";
+
+const initialState = {
+  amount: '',
+  category: '',
+  type: 'income',
+  date: new Date(),
+}
 
 const Form = () => {
   const classes = useStyles();
+  const [formData, setFormData] = useState(initialState);
+  const { addTransaction } = useContext(ExpenseTrackerContext);
+
+  const createTransaction = () => {
+    const transaction = { ...formData, amount: Number(formData.amount), id: uuidv4() }
+
+    addTransaction(transaction);
+    setFormData(initialState);
+  }
+
+  //console.log(formData);
 
   return (
     <Grid container spacing={2}>
@@ -24,7 +35,7 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Type</InputLabel>
-          <Select>
+          <Select value={formData.type} onChange={(e) => setFormData({ ...formData, type: e.target.value })}>
             <MenuItem value='income'>Income</MenuItem>
             <MenuItem value='expense'>Expense</MenuItem>
           </Select>
@@ -33,24 +44,19 @@ const Form = () => {
       <Grid item xs={6}>
         <FormControl fullWidth>
           <InputLabel>Category</InputLabel>
-          <Select>
+          <Select value={formData.category} onChange={(e) => setFormData({ ...formData, category: e.target.value })}>
             <MenuItem value='business'>Business</MenuItem>
             <MenuItem value='salary'>Salary</MenuItem>
           </Select>
         </FormControl>
       </Grid>
       <Grid item xs={6}>
-        <TextField type='number' label='Amount' fullWidth />
+        <TextField type='number' label='Amount' fullWidth value={formData.amount} onChange={(e) => setFormData({ ...formData, amount: e.target.value })} />
       </Grid>
       <Grid item xs={6}>
-        <TextField type='date' label='date' fullWidth />
+        <TextField type='date' label='date' fullWidth value={formData.date} onChange={(e) => setFormData({ ...formData, date: e.target.value })} />
       </Grid>
-      <Button
-        className={classes.button}
-        variant='outlined'
-        color='primary'
-        fullWidth
-      >
+      <Button className={classes.button} variant='outlined' color='primary' fullWidth onClick={createTransaction}>
         Create
       </Button>
     </Grid>
